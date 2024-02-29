cc6d0ee43ce9e5ed6894a1444b874b63a7bb6c19
jeison diaz palmera <jadiazp@indracompany.com>
2024-02-29 18:19:38 -0500

    init front

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  useEffect(() => {
+    const requestOptions:any = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("https://clase-8-react-back-dev-qkfz.2.us-1.fl0.io", requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }, [])
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
