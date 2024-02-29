c316a60354c3ff9ae7045257dea117b45353a10f
jeison diaz palmera <jadiazp@indracompany.com>
2024-02-29 18:41:34 -0500

    feat(OD2):create view card items

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,60 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export interface RootObject {
+  done: boolean
+  id: number
+  title: string
+}
 
+function App() {
+  const [data, setData] = useState<RootObject[]>([])
 
   useEffect(() => {
-    const requestOptions:any = {
-      method: "GET",
-      redirect: "follow"
-    };
-    
-    fetch("https://clase-8-react-back-dev-qkfz.2.us-1.fl0.io", requestOptions)
+    const requestOptions: any = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+
+    fetch('https://clase-8-react-back-dev-qkfz.2.us-1.fl0.io', requestOptions)
       .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
+      .then((result) => {
+        setData(result)
+
+        console.log(result)
+      })
+      .catch((error) => console.error(error))
   }, [])
-  
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='card'>
+        {data.map((item) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              border: '1px solid white',
+              gap: '10px',
+              borderRadius: '5px',
+            }}
+            key={item.id}
+          >
+            <h2>{item.title}</h2>
+            <p>{item.done ? 'Done' : 'Not done'}</p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
